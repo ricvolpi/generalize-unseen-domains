dbc6c5a0593bddbb8d27ecc414daa781b0814416
ricvolpi <riccardo.volpi@aol.com>
2018-09-13 11:07:59 +0200

    cleanup

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python main.py --gpu=0 --mode=train --exp_dir=./
-python main.py --gpu=0 --mode=test --exp_dir=./
+python main.py --gpu=$1 --mode=train --exp_dir=./
+python main.py --gpu=$1 --mode=test --exp_dir=./
 
 
 
